0fe64db509f697d1c5d5c9893dae547d28f62c55
Anthony Gelibert <anthony.gelibert@me.com>
2025-01-07 13:50:12 +0100

    ✨ Models to unserialize the coin information

--- wayne/models.py
+++ wayne/models.py
@@ -67,3 +67,57 @@ class InvestResult(BaseModel):
         """Structure of the capital at the end of the period."""
         return "liquidity" if self.positions_end == 0. else (f"{self.positions_end:.2f} x "
                                                              f"{self.capital_end / self.positions_end:.2f}")
+
+
+class Network(BaseModel):
+    """Network information."""
+
+    model_config = ConfigDict(frozen=True)
+
+    network: str
+    coin: str
+    withdraw_integer_multiple: Annotated[float, Field(alias="withdrawIntegerMultiple")]
+    is_default: Annotated[bool, Field(alias="isDefault")]
+    deposit_enable: Annotated[bool, Field(alias="depositEnable")]
+    withdraw_enable: Annotated[bool, Field(alias="withdrawEnable")]
+    deposit_desc: Annotated[str, Field(alias="depositDesc")]
+    withdraw_desc: Annotated[str, Field(alias="withdrawDesc")]
+    special_tips: Annotated[str, Field(alias="specialTips")]
+    special_withdraw_tips: Annotated[str, Field(alias="specialWithdrawTips")]
+    name: str
+    reset_address_status: Annotated[bool, Field(alias="resetAddressStatus")]
+    address_regex: Annotated[str, Field(alias="addressRegex")]
+    memo_regex: Annotated[str, Field(alias="memoRegex")]
+    withdraw_fee: Annotated[float, Field(alias="withdrawFee")]
+    withdraw_min: Annotated[float, Field(alias="withdrawMin")]
+    withdraw_max: Annotated[float, Field(alias="withdrawMax")]
+    withdraw_internal_min: Annotated[float, Field(alias="withdrawInternalMin")]
+    deposit_dust: Annotated[float | None, Field(alias="depositDust")] = None
+    min_confirm: Annotated[int, Field(alias="minConfirm")]
+    un_lock_confirm: Annotated[int, Field(alias="unLockConfirm")]
+    same_address: Annotated[bool, Field(alias="sameAddress")]
+    estimated_arrival_time: Annotated[int, Field(alias="estimatedArrivalTime")]
+    busy: bool
+    contract_address_url: Annotated[str | None, Field(alias="contractAddressUrl")] = None
+    contract_address: Annotated[str | None, Field(alias="contractAddress")] = None
+
+
+class CoinInfo(BaseModel):
+    """Coin information."""
+
+    model_config = ConfigDict(frozen=True)
+
+    coin: str
+    deposit_all_enable: Annotated[bool, Field(alias="depositAllEnable")]
+    withdraw_all_enable: Annotated[bool, Field(alias="withdrawAllEnable")]
+    name: str
+    free: float
+    locked: float
+    freeze: float
+    withdrawing: float
+    ipoing: float
+    ipoable: float
+    storage: float
+    is_legal_money: Annotated[bool, Field(alias="isLegalMoney")]
+    trading: bool
+    network_list: Annotated[list[Network], Field(alias="networkList")]
